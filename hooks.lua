78ee4a7ed4968b6e38805f23e1aff8cf289e4efb
HyptoHax <107750710+HyptoHax@users.noreply.github.com>
2023-08-03 20:07:07 +0200

    Update hooks.lua

---
+++ hooks.lua
@@ -0,0 +1,10 @@
+local function namecall(namecallname, callmethodd)
+    local method = getnamecallmethod()
+    local args = {...}
+
+    if not checkcaller() and self.Name == namecallname and method == callmethodd then
+        return wait(9e9);
+    end
+
+    return eventbypass(self, ...)
+end;
